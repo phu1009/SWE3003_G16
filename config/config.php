628d90622f520245b07c5f8a0fd9a25c7e9cea01
TaiTie132 <ttai130201@gmail.com>
2025-07-29 11:27:56 +0700

    config

--- config/config.php
+++ config/config.php
@@ -3,8 +3,8 @@ return [
     'db' => [
         'host'    => 'localhost',
         'name'    => 'lc_pms',
-        'user'    => 'lc_user',      // your user
-        'pass'    => 'secretPW',     // your pass
+        'user'    => 'tai',      // your user
+        'pass'    => 'pass',     // your pass
         'charset' => 'utf8mb4'
     ],
     'base_url' => '/lc-pms/public'   // adjust if different
